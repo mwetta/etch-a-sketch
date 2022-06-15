75e560b52a5c4b9a2579fb41485e80beb66eae33
Madeline <moon.madeline@gmail.com>
2022-06-15 14:31:43 -0500

    Add reset button and optimizations
    Add reset button to clear drawing from canvas. Add 'draw' function
    to set background of blocks to black on hover.
    
    Optimizations include:
    - Remove notes and unncessary CSS styles.
    - Add 'menu' div to remove full-size buttons
    - Separate out the prompt for user to set grid size to a separate
    function, which also checks that it is under 100

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-/*
-Instructions from TOP
-
-Follow the instructions atop Odin’s Recipes project to set up a Git repository for this project.
-    Create a webpage with a 16x16 grid of square divs.
-        Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
-        It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
-        There are several different ways to make the divs appear as a grid (versus just one on each line). Feel free to use any or play with each of them:
-            float/clear
-            inline-block
-            flexbox
-            CSS Grid
-    Be careful with borders and margins, as they can adjust the size of the squares!
-    “OMG, why isn’t my grid being created???”
-    Did you link your CSS stylesheet?
-    Open your browser’s developer tools.
-    Check if there are any errors in the JavaScript console.
-    Check your “elements” pane to see if the elements have actually shown up but are somehow hidden.
-    Go willy-nilly and add console.log statements in your JavaScript to see if it’s actually being loaded.
-Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
-    Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. You can set up event listeners for either of those events as a starting point.
-    There are multiple ways to change the color of the divs, including:
-        adding a new class to the div. 
-        changing the div’s background color using JavaScript.
-Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
-    Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
-    Also check out prompts.
-    You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
-(Optional): Instead of just changing the color of a square from black to white (for example), have each pass through with the mouse change it to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
-Push your project to GitHub!
-*/
-
 const container = document.querySelector('#container');
 let gridSize = 16; //default
 resizeGrid(16);
@@ -38,9 +6,25 @@ document.querySelector('#button').addEventListener('click', function() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
       };
-    gridSize = prompt("What Size Grid?,''");
-    resizeGrid(gridSize);
+    setGridSize();
 });
+
+document.querySelector('#reset').addEventListener('click', function() {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach((block) => {
+        block.classList.remove('draw');});
+});
+
+
+function setGridSize() {
+    let userGrid = prompt("What Size Grid?"); //need to set maximum to 100
+    if (userGrid > 100) {
+        return setGridSize()
+    } else {
+        gridSize = userGrid;
+        return resizeGrid(gridSize)
+    }
+}
 
 function resizeGrid(gridSize) {
     for (let i = 0; i < gridSize; i++){
@@ -51,9 +35,21 @@ function resizeGrid(gridSize) {
 
         for (let c = 0; c < gridSize; c++){
             let block = document.createElement('div');
-            block.setAttribute('id',`block-${i}`);
+            block.setAttribute('id',`block-${c}`);
             block.classList.add('block');
             column.appendChild(block);
         };
     };
+
+    draw();
 }
+
+function draw() {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach((block) => {
+      block.addEventListener('pointerover', () => {
+        block.classList.add('draw')
+      });
+    });
+}
+
